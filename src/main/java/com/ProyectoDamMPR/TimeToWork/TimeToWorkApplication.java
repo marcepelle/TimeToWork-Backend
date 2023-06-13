032e06b6e7e5a6b3622cc723cf78add0d8d2d655
marcepellerinal <marcepellerinal@gmail.com>
2023-06-13 18:59:44 +0200

    update timetowork

--- src/main/java/com/ProyectoDamMPR/TimeToWork/TimeToWorkApplication.java
+++ src/main/java/com/ProyectoDamMPR/TimeToWork/TimeToWorkApplication.java
@@ -6,17 +6,19 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.WebApplicationInitializer;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+@RestController
 @SpringBootApplication
 public class TimeToWorkApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(TimeToWorkApplication.class, args);
 	}
 
-	@RequestMapping(path = "/")
+	@GetMapping("/")
 	@ResponseBody
 	String home(){
 		return "Hello World";
