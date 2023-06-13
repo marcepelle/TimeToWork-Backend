b97791576861a61087f53056422c2205548f34b1
marcepellerinal <marcepellerinal@gmail.com>
2023-06-13 19:06:26 +0200

    controller de prueba para el despliegue

--- src/main/java/com/ProyectoDamMPR/TimeToWork/TimeToWorkApplication.java
+++ src/main/java/com/ProyectoDamMPR/TimeToWork/TimeToWorkApplication.java
@@ -3,24 +3,11 @@ package com.ProyectoDamMPR.TimeToWork;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
-import org.springframework.stereotype.Controller;
-import org.springframework.web.WebApplicationInitializer;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+
 @SpringBootApplication
 public class TimeToWorkApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(TimeToWorkApplication.class, args);
-	}
-
-	@GetMapping("/")
-	@ResponseBody
-	String home(){
-		return "Hello World";
 	}
 }
---
+++ src/main/java/com/ProyectoDamMPR/TimeToWork/controlador/IndexContoller.java
@@ -0,0 +1,14 @@
+package com.ProyectoDamMPR.TimeToWork.controlador;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class IndexContoller {
+
+    @RequestMapping(value = "/", method = RequestMethod.GET)
+    public String index(){
+        return "Hello World";
+    }
+}
