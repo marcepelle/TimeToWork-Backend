4a80071432dd913450402ebb5eef88d3a367d808
marcepellerinal <marcepellerinal@gmail.com>
2023-06-13 18:07:10 +0200

    updating pom, servlet y main

--- src/main/java/com/ProyectoDamMPR/TimeToWork/ServletInitializer.java
+++ src/main/java/com/ProyectoDamMPR/TimeToWork/ServletInitializer.java
@@ -4,8 +4,7 @@ import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.web.WebApplicationInitializer;
 
-public class
-ServletInitializer extends SpringBootServletInitializer implements WebApplicationInitializer {
+public class ServletInitializer extends SpringBootServletInitializer implements WebApplicationInitializer {
 	@Override
 	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
 		return application.sources(TimeToWorkApplication.class);
--- src/main/java/com/ProyectoDamMPR/TimeToWork/TimeToWorkApplication.java
+++ src/main/java/com/ProyectoDamMPR/TimeToWork/TimeToWorkApplication.java
@@ -5,20 +5,20 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.WebApplicationInitializer;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 @SpringBootApplication
-public class TimeToWorkApplication extends SpringBootServletInitializer {
+public class TimeToWorkApplication extends SpringBootServletInitializer implements WebApplicationInitializer {
 	public static void main(String[] args) {
 		SpringApplication.run(TimeToWorkApplication.class, args);
 	}
 
-	@RequestMapping("/")
+	@RequestMapping(path = "/")
 	@ResponseBody
 	String home(){
 		return "Hello World";
 	}
-
 }
