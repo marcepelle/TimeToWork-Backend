d5af932035aea261ffeb76d1d28b96b7ec92831c
marcepellerinal <marcepellerinal@gmail.com>
2023-06-13 17:12:16 +0200

    updating springbootservletinitializer

--- src/main/java/com/ProyectoDamMPR/TimeToWork/TimeToWorkApplication.java
+++ src/main/java/com/ProyectoDamMPR/TimeToWork/TimeToWorkApplication.java
@@ -3,13 +3,14 @@ package com.ProyectoDamMPR.TimeToWork;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 @SpringBootApplication
-public class TimeToWorkApplication {
+public class TimeToWorkApplication extends SpringBootServletInitializer {
 	public static void main(String[] args) {
 		SpringApplication.run(TimeToWorkApplication.class, args);
 	}
